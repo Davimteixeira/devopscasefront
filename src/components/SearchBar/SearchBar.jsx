7e4a6e7f578e4b4f0cd19679cdde37fef7ce72dd
Davimteixeira <daviteixeira077@gmail.com>
2024-12-07 19:31:28 -0300

    change: teste de deploy

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -20,7 +20,7 @@ function SearchBar({ onSearch }) {
       <input
         className="search-input"
         type="text"
-        placeholder="enter Pokémon Name"
+        placeholder="enterr Pokémon Name"
         value={pokemonSearchName}
         onChange={(e) => setPokemonSearchName(e.target.value)}
         onKeyDown={handleKeyDown}
